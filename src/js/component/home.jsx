c796b89a5f5d1baba83e0948bd3822cc400f9aa0
MarcosEstPam <marcosepamias@gmail.com>
2024-08-18 16:50:14 +0000

    avances

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,24 +2,21 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import ToDoList from "./toDoList";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<><div className="d-flex flex-column min-vh-100">
+			<h1 className="d-flex flex-column align-items-center justify-content-center title">todos</h1>
+			<ToDoList/>
+			<footer className="footer mt-auto py-3 bg-light text-center">
+                <div className="container">
+                    <span className="text-muted">© 2024 Your Company. All rights reserved.</span>
+                </div>
+            </footer>			
 		</div>
+		</>
 	);
 };
 
